02572a74f4caae670a59876987b1de9d1eaca442
Calista Mayer <calistammayer@gmail.com>
2023-04-28 11:18:17 -0400

    still need to add tests

--- Graph.py
+++ Graph.py
@@ -29,7 +29,7 @@ class Graph:
         self._nbrs[u].add(v)
         self.E.add((u, v, wt))
 
-    def remove_edge(self, u, v):
+    def remove_edge(self, u, v, wt=0):
         """Removes an edge from the graph"""
         for e in self.E:
             if e[0] == u and e[1] == v:
@@ -48,17 +48,108 @@ class Graph:
         """Returns neighbors of the graph"""
         return iter(self._nbrs[v])
 
+    def fewest_flights(self, city):
+        tree_dict = {}
+        tree_nbr_flights = {}
+        to_visit = [(None, city)]
+        while to_visit:
+            a, b = to_visit.pop(0)
+            if b not in tree_dict:
+                tree_dict[b] = a
+                if a is None:
+                    tree_nbr_flights[b] = 0
+                else:
+                    tree_nbr_flights[b] = 1 + tree_nbr_flights[a]
+                for n in self.nbrs(b):
+                    to_visit.append((b, n))
+        return tree_dict, tree_nbr_flights
+
+    def _weight(self, u, v):
+        for edge in self.E:
+            if (edge[0] == u and edge[1] == v) or (edge[0] == v and edge[1] == u):
+                return edge[2]
+        return 0
+
+    def shortest_path(self, city):
+        dict_distance = {u: float("inf") for u in self.V}
+        dict_distance[city] = 0
+        trav_tree = {city: None}
+
+        dict_nbr_distance = {city: 0}
+
+        while dict_nbr_distance:
+            current_city, lowest_value = list(dict_nbr_distance.items())[0]
+            for city in dict_nbr_distance.keys():
+                if dict_nbr_distance[city] < lowest_value:
+                    lowest_value = dict_nbr_distance[city]
+                    current_city = city
+
+            del dict_nbr_distance[current_city]
+
+            for neighbor in self.nbrs(current_city):
+
+                dist_to_city = self._weight(current_city, neighbor)
+
+                if dict_distance[neighbor] > dict_distance[current_city] + dist_to_city:
+                    trav_tree[neighbor] = current_city
+                    dict_distance[neighbor] = dict_distance[current_city] + dist_to_city
+                    dict_nbr_distance[neighbor] = int(dict_distance[neighbor])
+
+            return trav_tree, dict_distance
+
+    def minimum_salt(self, city):
+        tree_dict = {}
+        dict_dist = {}
+        toVisit = [(None, city, 0)]
+        while toVisit:
+            prioritized = toVisit.pop()
+            a = prioritized[0]
+            b = prioritized[1]
+            if b not in tree_dict:
+                tree_dict[b] = a
+                if a is None:
+                    dict_dist[b] = 0
+                else:
+                    dict_dist[b] = self._weight(a, b)
+                for neighbor in self.nbrs(b):
+                    toVisit = self._sort_priority(
+                        (b, neighbor, self._weight(b, neighbor)), toVisit
+                    )
+        return tree_dict, dict_dist
+
+    def _sort_priority(self, entry, L):
+        if len(L) == 0:
+            return [entry]
+        index = len(L)
+        for i in range(len(L)):
+            if entry[2] > L[i][2]:
+                index = i
+                break
+        newList = L[:index]
+        newList.append(entry)
+        newList = newList + L[index:]
+        return newList
+
 
 if __name__ == "__main__":
     G = Graph({"Boston", "New York", "Philadelphia"})
+    g = Graph(
+        {"Chicago", "Boston", "Philadelphia", "Cape Cod", "Stockholm"},
+        {
+            ("Chicago", "Boston", 400),
+            ("Boston", "Cape Cod", 30),
+            ("Cape Cod", "Stockholm", 4000),
+            ("Chicago", "Philadelphia", 300),
+            ("Philadelphia", "Cape Cod", 500),
+        },
+    )
     G.add_edge("Boston", "New York", 500)
     G.add_edge("Boston", "Philadelphia", 1000)
     G.add_edge("New York", "Philadelphia", 4000)
-    print(G.E)
-    print(G._nbrs)
-    print(G.V)
-    G.remove_edge("Boston", "New York")
-    print(G.E)
-    print("\n")
-    G.remove_vertex("Boston")
-    print(G.E)
+    # print(g.fewest_flights("Philadelphia")[0])
+    dic, dist = g.fewest_flights("Philadelphia")
+    print(dist)
+    print(dic)
+    print("\n)")
+    # print(g.fewest_flights("Chicago"[0]))
+    # print(G.shortest_path("Boston"))
--- TestGraph.py
+++ TestGraph.py
@@ -18,31 +18,141 @@ class test_Graph(unittest.TestCase):
 
         """
 
-    # TODO: Add unittests for public interface of Graph class (except traversal algs)
+        # TODO: Add unittests for public interface of Graph class (except traversal algs)
+        self.g = Graph(
+            {"Chicago", "Boston", "Philadelphia", "Cape Cod", "Stockholm"},
+            {
+                ("Chicago", "Boston", 400),
+                ("Boston", "Cape Cod", 30),
+                ("Cape Cod", "Stockholm", 4000),
+                ("Chicago", "Philadelphia", 300),
+                ("Philadelphia", "Cape Cod", 500),
+            },
+        )
+
+    def test_add_vertex(self):
+        self.g.add_vertex("Washington")
+        self.assertTrue("Washington" in self.g.V)
+        self.assertTrue("Washington" in self.g._nbrs.keys())
+
+    def test_remove_vertex(self):
+        self.g.remove_vertex("Boston")
+        self.assertTrue("Boston" not in self.g.V)
+        self.assertTrue("Boston" not in self.g._nbrs.keys())
+
+        self.assertTrue("Boston" not in self.g._nbrs["Chicago"])
+        self.assertTrue("Boston" not in self.g._nbrs["Cape Cod"])
+
+        notInEdges = True
+        for edge in self.g.E:
+            if edge[0] == "Boston" or edge[1] == "Boston":
+                notInEdges = False
+        self.assertTrue(notInEdges)
+
+    def test_add_edge(self):
+        self.g.add_edge("Boston", "Stockholm", 5000)
+        self.assertTrue(("Boston", "Stockholm", 5000) in self.g.E)
+
+        self.assertTrue("Boston" in self.g._nbrs["Stockholm"])
+        self.assertTrue("Stockholm" in self.g._nbrs["Boston"])
+
+    def test_remove_edge(self):
+        self.g.remove_edge("Boston", "Cape Cod")
+        self.assertTrue(("Boston", "Cape Cod", 30) not in self.g.E)
+
+        self.assertTrue("Boston" not in self.g._nbrs["Cape Cod"])
+        self.assertTrue("Cape Cod" not in self.g._nbrs["Boston"])
+
+    def test_nbrs(self):
+        self.num_graph = Graph({3, 7, 2, 4, 5}, {(2, 4, 1), (3, 2, 1), (5, 2, 1)})
+        i = 0
+        neighbors = [3, 4, 5]
+        for neighbor in self.num_graph.nbrs(2):
+            self.assertEqual(neighbor, neighbors[i])
+            i += 1
 
 
 class test_GraphTraversal(unittest.TestCase):
     # Create a graph `self.g` that you can use in your other unittests. Include ASCII art.
+
     def setUp(self):
         """ADD DOCSTRING"""
 
+        f"""
+                   400
+        Chicago ----------- Boston
+           |                   |
+           |300                |30
+           |                   |
+        Philadelphia -------Cape Cod------Stockholm
+                        500           4000
+
+        """
+
+        # TODO: Add unittests for public interface of Graph class (except traversal algs)
+        self.g = Graph(
+            {"Chicago", "Boston", "Philadelphia", "Cape Cod", "Stockholm"},
+            {
+                ("Chicago", "Boston", 400),
+                ("Boston", "Cape Cod", 30),
+                ("Cape Cod", "Stockholm", 4000),
+                ("Chicago", "Philadelphia", 300),
+                ("Philadelphia", "Cape Cod", 500),
+            },
+        )
+
     # TODO: Which alg do you use here, and why?
-    # Alg:
-    # Why:
+    # Alg: Breadth-First Search
+    # Why: Finds path with the fewest number of edges
     def test_fewest_flights(self):
-        """ADD DOCSTRING"""
+        """Tests for fewest flights."""
+        # self.g.fewest_flights("Philadelphia")
+        # self.assertEqual(
+        #     self.g.fewest_flights(
+        #         "Philadelphia",
+        #         {
+        #             "Philadelphia": None,
+        #             "Chicago": "Philadelphia",
+        #             "Cape Cod": "Philadelphia",
+        #             "Boston": "Chicago",
+        #             "Stockholm": "Cape Cod",
+        #         },
+        #     )
+        # )
+        # self.g.fewest_flights("Philadelphia")
+        dic, dist = self.g.fewest_flights("Philadelphia")
+        self.assertEqual(
+            dic,
+            {
+                "Philadelphia": 0,
+                "Chicago": 1,
+                "Cape Cod": 1,
+                "Boston": 2,
+                "Stockholm": 2,
+            },
+        )
+        self.assertEqual(
+            dist,
+            {
+                "Philadelphia": None,
+                "Chicago": "Philadelphia",
+                "Cape Cod": "Philadelphia",
+                "Boston": "Chicago",
+                "Stockholm": "Cape Cod",
+            },
+        )
 
     # TODO: Which alg do you use here, and why?
-    # Alg:
-    # Why:
+    # Alg: Dijkstra's Algorithm
+    # Why: Finds shortest path (including weight) between two vertices
     def test_shortest_path(self):
-        """ADD DOCSTRING"""
+        """Tests for shortest path"""
 
     # TODO: Which alg do you use here, and why?
-    # Alg:
-    # Why:
+    # Alg: Prim's Algorithm
+    # Why: Finds shortest path between all vertices
     def test_minimum_salt(self):
-        """ADD DOCSTRING"""
+        """Finds shortest path between all cities"""
 
 
 unittest.main()
